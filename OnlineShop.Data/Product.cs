5a28c0a656a691e424a9dc330681cc035f87fcd9
CECO <cvetomir97@abv.bg>
2023-12-15 11:01:09 +0200

    Created Blazor Web Assembly application.

---
+++ OnlineShop.Data/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineShop.Data
+{
+    public class Product
+    {
+        public int? ProductId { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public decimal UnitPrice { get; set; }
+
+        public int UnitInStock { get; set; }
+
+        public DateTime DeliveryOn { get; set; }
+
+        public string ImageUrl { get; set; } = null!;
+    }
+}
