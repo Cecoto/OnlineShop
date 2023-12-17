5cc5aa255100bc8b8ccdcd155acc1d1f597dd4eb
CECO <cvetomir97@abv.bg>
2023-12-17 17:08:57 +0200

    Added TelerikNotification component to button onClick event.

--- OnlineShop/Components/ProductCard.razor.cs
+++ OnlineShop/Components/ProductCard.razor.cs
@@ -2,11 +2,24 @@
 {
     using Microsoft.AspNetCore.Components;
     using OnlineShop.Data;
+    using Telerik.Blazor.Components;
+    using Telerik.Blazor;
+    using Telerik.SvgIcons;
 
     public partial class ProductCard
     {
         [Parameter]
         public Product Product { get; set; } = null!;
-     
+
+        public TelerikNotification NotificationReference { get; set; } = null!;
+
+        public void AddNotification()
+        {
+            NotificationReference.Show(new NotificationModel()
+            {
+                Text = "Product added to the shopping card.",
+                CloseAfter = 3000,
+            }) ;
+        }
     }
 }
