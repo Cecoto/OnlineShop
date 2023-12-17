7d33da241c8476e13bf77bec6d88b217d002f51c
CECO <cvetomir97@abv.bg>
2023-12-17 17:39:16 +0200

    Removing the service project, because it was not working.

---
+++ OnlineShop.Services/Contracts/IProductService.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineShop.Services.Contracts
+{
+    using OnlineShop.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IProductService
+    {
+        Task<Product[]> GetAllProductsAsync();
+    }
+}
